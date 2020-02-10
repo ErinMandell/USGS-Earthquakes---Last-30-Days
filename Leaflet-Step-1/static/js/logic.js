a7b8cede03fd9755b33b594cd5c32822208fa5f0
ErinMandell <ErinMandell@users.noreply.github.com>
2020-02-09 21:08:01 -0600

    begin legend

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -29,33 +29,48 @@ function createMap(earthquakes) {
         layers: [streetmap, earthquakes]
     });
     
-    // earthquakeMarkers
-    //  .addTo(map)
-    //  .bindPopup("<h3>" + earthquakeInfo[i].properties.place + "<hr>Magnitude: " + earthquakeInfo[i].properties.mag +
-    //  "</h3><p>" + new Date(earthquakeInfo[i].properties.time) + "</p>");
-
     L.control.layers(baseMaps, overlayMaps, {
         collapsed: false
     }).addTo(map);
 
+
+
+    // Create a legend to display information about our map
+    // When the layer control is added, insert a div with the class of "legend"
+    // Add the info legend to the map
+    
+    // var info = L.control({
+    //     position: "bottomleft"
+    // });
+  
+    // info.onAdd = function() {
+    //     var div = L.DomUtil.create("div", "legend");
+    //     return div;
+    // };
+
+    // info.addTo(map)
+
 }
 
+
+
 function createMarkers(response) {
-    console.log(response);
+    // console.log(response);
+
     var earthquakeInfo = response.features;
     console.log(earthquakeInfo);
 
     for (var i=0; i < earthquakeInfo.length; i++) {
 
         var location = [earthquakeInfo[i].geometry.coordinates[1], earthquakeInfo[i].geometry.coordinates[0]];
-        console.log(location);
+        // console.log(location);
 
         var color = "";         
         if (earthquakeInfo[i].properties.mag < 1) {
-            color = "#ffffcc";
+            color = "#7CFC00";
         }
         else if (earthquakeInfo[i].properties.mag <=2) {
-            color = "#ffd700";
+            color = "#ffff99";
         }
         else if (earthquakeInfo[i].properties.mag <=3) {
             color = "#ffa500";
@@ -67,14 +82,13 @@ function createMarkers(response) {
             color = "#ff6347"
         }
         else {
-            color = "#ff0000";
+            color = "#cc3399";
         }
 
         var newCircle = L.circle(location, {
-            fillOpacity: .85,
+            fillOpacity: .95,
             color: color,
-            // fillColor: getColor(feature.properties.mag),
-            radius: earthquakeInfo[i].properties.mag * 50000
+            radius: earthquakeInfo[i].properties.mag * 30000
           });
 
         newCircle
@@ -83,13 +97,57 @@ function createMarkers(response) {
 
         earthquakeMarkers.push(newCircle);
         // console.log(earthquakeMarkers);
+
+
+
     }
+    
+    function getColor(mag) {
+        console.log(mag);
+        return  mag < 1 ? "#ffffcc":
+                mag < 2 ? "#ffd700":
+                mag < 3 ? "#ffa500":
+                mag < 4 ? "#ff8c00":
+                mag < 5? "#ff6347":
+                        "#ff0000";
+                break;
+    }
+
+
+    var legend = L.control({position: 'bottomleft'});
+
+    legend.onAdd = function() 
+    {
+        var div = L.DomUtil.create('dev', 'info legend');
+        var mag = [0, 1, 2, 3, 4, 5];
+        var colors = ["#ffffcc", "#ffd700", "#ffa500", "#ff8c00", "#ff6347", "#ff0000"];
+        var labels = [];
+
+        var legendInfo = "<h1>Magnitude Scale</h1>" +
+            "<div class=\"labels\">" +
+            "<div class=\""
+
+        for (var i = 0; i < mag.length; i++) 
+        {
+            div.innerHTML +=
+            '<i style="background:' + getColor(mag[i] + 1) + '"></i> ' +
+            mag[i] + (mag[i + 1] ? '&ndash;' + mag[i + 1] + '<br>' : '+');
+        }
+
+        return div;
+    };
+
+    legend.addTo(map);
 
     createMap(L.layerGroup(earthquakeMarkers));
 }
 
+
+
+
 var queryUrlPartial = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson"
+var queryUrlWeekly = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 var queryUrlFull = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson"
 var earthquakeMarkers = [];
 
-d3.json(queryUrlPartial, createMarkers);
+d3.json(queryUrlWeekly, createMarkers);
